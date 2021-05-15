f97dbe0f4bd858f8a5d13543820e29127a25c30a
Suvash Bhandari <40908632+bsubash2054@users.noreply.github.com>
2021-05-15 13:40:17 +0545

    Comment descriptions added to main.py

--- main.py
+++ main.py
@@ -17,22 +17,22 @@ watermark_pdf = PdfFileReader(open("temp/watermark.pdf", "rb")) #open watermark 
 watermark_page = watermark_pdf.getPage(0) #get PDF Page of Watermark
 
 
-for filename in os.listdir(directory):   #loop via all pdfs in in-pdf
-    if filename.endswith(".pdf"):
-        input_file = "in-pdf/"+filename
-        output_file = "out-pdf/"+filename
+for filename in os.listdir(directory):   #loop via all pdfs placed in the directory 'in-pdf'
+    if filename.endswith(".pdf"): #Take only pdf files to watermark
+        input_file = "in-pdf/"+filename #place your files to watermark in this directory
+        output_file = "out-pdf/"+filename #Watermarked files appear here after being watermarked
 
         with open(input_file, 'rb') as f:
             pdf_reader = PdfFileReader(f)
             number_of_pages = pdf_reader.getNumPages()
             output = PdfFileWriter()
-            for x in range(number_of_pages):
-                page_temp = pdf_reader.getPage(x)
-                page_temp.mergePage(watermark_page)
+            for x in range(number_of_pages): #Loop through each page of the pdf file
+                page_temp = pdf_reader.getPage(x) #Get the current page to watermark
+                page_temp.mergePage(watermark_page) #Apply watermark to a particular page
                 output.addPage(page_temp)
                 print(str(x)+" Pages of " + str(number_of_pages) + " of File :"+filename)
             with open(output_file, "wb") as merged_file:
-                output.write(merged_file)
+                output.write(merged_file) #Write watermarked file to the directory 'out-pdf'
         print("File WaterMarked:"+filename)
     else:
         continue
